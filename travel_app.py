fdc63d3793718376f8e43b19807e12b135bd6b7c
juneferre <juneferre23@gmail.com>
2025-04-15 21:13:36 -0600

    trying this again??

--- travel_app.py
+++ travel_app.py
@@ -9,9 +9,7 @@ import numpy as np
 from io import StringIO
 
 
-file_id = st.secrets["google_drive"]["file_id"]
-url = f"https://drive.google.com/uc?id={file_id}"
-
+url = st.secrets["data"]["csv_url"]
 df = pd.read_csv(url)
 
 
